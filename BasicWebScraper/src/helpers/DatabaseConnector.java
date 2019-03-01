4015d2e74e8ab51ddb1f91c13ec8e435577449ef
leongshengmin <leongshengmin@gmail.com>
2019-03-01 18:12:56 +0800

    add db connector

---
+++ BasicWebScraper/src/helpers/DatabaseConnector.java
@@ -0,0 +1,28 @@
+package helpers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+
+    private Connection dbConnection;
+
+    public DatabaseConnector(String connectionUrl) {
+        // change to env
+        try {
+            dbConnection = DriverManager.getConnection(connectionUrl);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int insertUpdate(String query) throws SQLException {
+        return dbConnection.createStatement().executeUpdate(query);
+    }
+
+    public ResultSet selectQuery(String query) throws SQLException {
+        return dbConnection.createStatement().executeQuery(query);
+    }
+}

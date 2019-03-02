e520baad6e75ef2e11cc7fb1686dfb4694f87c20
leongshengmin <leongshengmin@gmail.com>
2019-03-02 15:32:16 +0800

    remove unrelated files

--- 2019_y2s2/CS2105/Assignments/Assign1/ServerSocket.py
+++
@@ -1,8 +0,0 @@
-from socket import socket, AF_INET, SOCK_STREAM
-
-class ServerSocket:
-
-    def __init__(self, port_num):
-        self.socket = socket(AF_INET, SOCK_STREAM)
-        self.port_num = int(port_num)
-        self.socket.bind(('', self.port_num))

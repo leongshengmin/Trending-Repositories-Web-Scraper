35ca7bfc449b6586955fa1e149bfe46452ea8f2c
leongshengmin <leongshengmin@gmail.com>
2019-03-02 12:47:49 +0800

    set default root url

--- BasicWebScraper/src/Spider.java
+++ BasicWebScraper/src/Spider.java
@@ -1,23 +1,20 @@
 import java.util.HashSet;
-import java.util.LinkedList;
 
 import helpers.Crawler;
 import helpers.DocumentParser;
 import org.jsoup.nodes.Document;
 
 public class Spider {
+    private static final String ROOT_URL ="https://github.com/trending?since=daily";
 
     private HashSet<String> visitedUrls;
-    private LinkedList<String> linksToVisit;
-    private String rootUrl;
 
-    Spider(String rootUrl) {
-        this.rootUrl = rootUrl;
+    Spider() {
         this.visitedUrls = new HashSet<>();
     }
 
     public void query() {
-        query(rootUrl);
+        query(ROOT_URL);
     }
 
     private void query(String link) {
@@ -33,8 +30,7 @@ public class Spider {
     }
 
     public static void main(String[] args) {
-        String rootUrl = "https://github.com/trending?since=daily";
-        Spider spider = new Spider(rootUrl);
+        Spider spider = new Spider();
         spider.query();
     }
 }
